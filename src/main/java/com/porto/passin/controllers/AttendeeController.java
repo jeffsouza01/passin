08c2887e3291ef91ab50925107041406edeea290
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-04-03 19:53:58 -0300

    creating controllers

---
+++ src/main/java/com/porto/passin/controllers/AttendeeController.java
@@ -0,0 +1,17 @@
+package com.porto.passin.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendee")
+public class AttendeeController {
+
+
+    @GetMapping
+    public ResponseEntity<String> getTest(){
+        return ResponseEntity.ok("Success in Test");
+    }
+}
